f875a3ac7b264381395bfc3910177f8b26ed3dd8
Your Name <you@example.com>
2024-12-04 08:11:49 +0100

    formular v externom subore

--- formular.php
+++ formular.php
@@ -6,12 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="formular.php" name="registracia" method="post" >
-        <input type="text" name="meno" placeholder="Zadajte meno" required> <br>
-        <input type="password" name="heslo" placeholder="Zadajte heslo" required> <br>
-        <input type="submit" name="tlacidlo" value="Odoslať"> <br>
-    </form>
-
 
     <?php
         $pole = ["Jozo", "Jano", "Lucia"];
@@ -26,9 +20,7 @@
                 else {
                     die("chyba nespravne pouzivatelske meno alebo heslo");
                 }
-                
-     
-                
+                         
             }
 
         }

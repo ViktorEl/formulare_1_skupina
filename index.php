40f8e4eb14c496210658aae7729ce99b9a084bb7
Your Name <you@example.com>
2025-01-10 13:40:19 +0100

    Vytvorenie skriptu pre formular

--- index.php
+++ index.php
@@ -6,63 +6,51 @@
     <title>Document</title>
 </head>
 <body>
+    <h2>Učíme sa skryť formulár</h2>
+
     <form action="index.php" method="post">
-        <input type="checkbox" name="jazyk[]" value="1"> <label>PHP</label> <br>
-        <input type="checkbox" name="jazyk[]" value="2"> <label>Java</label> <br>
-        <input type="checkbox" name="jazyk[]" value="3"> <label>Python</label> <br>
-        <input type="checkbox" name="jazyk[]" value="4"> <label>C++</label> <br>
-        <input type="submit" name="tlacidlo" value="odoslať">
+        <input type="checkbox" name="znacka[]" value="1"> <label>Mercedes</label> <br>
+        <input type="checkbox" name="znacka[]" value="2"> <label>Audi</label> <br>
+        <input type="checkbox" name="znacka[]" value="3"> <label>BMW</label> <br>
+        <input type="submit" name="tlacidlo" value="Odoslať">
     </form>
-
-
-
 
     <?php
         if(isset($_POST["tlacidlo"])) {
-            if(isset($_POST["jazyk"])) {
-                if(count($_POST["jazyk"]) < 2 || count($_POST["jazyk"]) > 3) {
-                    die("musite vybrat minimalne 2 a maximalne 3 moznosti");
+            if(isset($_POST["znacka"])) {
+                $pole = $_POST["znacka"];
+                $pocet = count($pole);
+                if($pocet != 2) {
+                    die("musite vybrať 2 možnosti ");
                 }
-                $pole = $_POST["jazyk"];
+
                 foreach($pole as $prvok) {
                     if($prvok == "1") {
-                        echo "vybrali ste si PHP";
+                        echo "vybrali ste si Mercedes"; 
                         echo "<br>";
                     }
                     elseif($prvok == "2") {
-                        echo "vybrali ste si Javu";
+                        echo "vybrali ste si Audi";
                         echo "<br>";
                     }
                     elseif($prvok == "3") {
-                        echo "vybrali ste si Python";
-                        echo "<br>";
-                    }
-                    else {
-                        echo "vybrali ste si C++";
+                        echo "vybrali ste si BMW";
                         echo "<br>";
                     }
                 }
 
 
-
-            }
-            else {
-                die("nevybrali ste ziadnu moznost");
             }
 
         }
-   
+
+
 
 
     ?>
 
 
 
-
-
+    
 </body>
-
-
-
-
 </html>
